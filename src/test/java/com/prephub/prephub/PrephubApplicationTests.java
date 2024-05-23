90f58d2a16580a2eb3414fa9e41e4ec0c109e491
BiggestM <muhamed.mehmedovic@stu.ssst.edu.ba>
2024-05-23 21:42:25 +0200

    first commit

---
+++ src/test/java/com/prephub/prephub/PrephubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prephub.prephub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrephubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
